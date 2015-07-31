45f37086868a028620a41e685a593ea35e8cd5af
Christophe Rosset <tophe@topheman.com>
2015-08-01 01:07:39 +0200

    pipe number-sequence stream to stdout

---
+++ experiments/stdout.pipe.number-sequence.js
@@ -0,0 +1,25 @@
+var NumberSequence = require('../libs/number-sequence');
+
+var Transform = require('stream').Transform;
+var parser = new Transform({objectMode:true});
+
+//also works
+//parser._readableState.objectMode = false;
+//parser._writableState.objectMode = true;
+
+parser._transform = function(buffer, encoding, next){
+  this.push('counting ... ' + buffer.toString()+'\n');
+  next();
+}
+
+var numberSequence = new NumberSequence(300,10);
+
+numberSequence
+  .pipe(parser)
+  .pipe(process.stdout);
+
+numberSequence.on('end',function(chunk){
+  process.stdout.write('ended at: ' + chunk + '\n');
+})
+
+numberSequence.start();
